65d68453e92fe15446e5e62ee191b9bfd8274353
Angie-Santos <santosangie79@gmail.com>
2022-10-17 10:05:36 -0500

    función para verificar ruta y volver la ruta de relativa a absoluta hecha close #2

---
+++ src/verify-path.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-console */
+const fs = require('fs');
+const pathNode = require('path');
+
+function normalizePath(path) {
+  return pathNode.resolve(path);
+}
+
+function verifyPath(path) {
+  try {
+    const stats = fs.statSync(path);
+    return stats.isFile() ? 'file' : 'directory';
+  } catch (err) {
+    // console.error('path does not exist or is broken');
+    return false;
+  }
+}
+
+module.exports = {
+  verifyPath, normalizePath,
+};
